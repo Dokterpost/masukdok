1b1cb746ea545a491dafdd27d65d739d8cd08cfc
Dokterpost <dokterpot5@gmail.com>
2022-03-01 17:20:30 +0700

    6

--- koneksion.php
+++ koneksion.php
@@ -1,11 +1,11 @@
 <?php
 
-$server = "174.138.29.9";
+$db = "174.138.29.9";
 $user = "uhzahqfmdx";
 $password = "Ngj6WtTnwA";
 $nama_database = "uhzahqfmdx";
 
-$db = mysqli_connect($server, $user, $password, $nama_database);
+$db = mysqli_connect($db, $user, $password, $nama_database);
 
 if( !$db ){
     die("Gagal terhubung dengan database: " . mysqli_connect_error());
