5436af147e2ea4e1f98f33c196167ec8496622a8
Dokterpost <dokterpot5@gmail.com>
2022-03-01 17:28:44 +0700

    9

---
+++ ozaz/pages/koneksion.php
@@ -0,0 +1,17 @@
+<?php
+
+$server = "174.138.29.9";
+$user = "uhzahqfmdx";
+$password = "Ngj6WtTnwA";
+$nama_database = "uhzahqfmdx";
+
+$db = mysqli_connect($server, $user, $password, $nama_database);
+
+if( !$db ){
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
+}
+else{
+    printf("sukses");
+}
+
+?>
